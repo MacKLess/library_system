aa49c8512c1c3faead329bde201fc8bee442c6c5
Kristen Kulha and LDMacKrell <kristen@>
2017-09-20 11:56:03 -0700

    update app to create an instance of a patron.

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,10 +1,10 @@
 class Patron
   attr_accessor(:id, :name)
 
-  def initalize(attributes)
+  def initialize(attributes)
     @id = attributes.fetch(:id)
     @name = attributes.fetch(:name)
   end
 
 
-end    
+end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe(Patron) do
+  describe('#initialize') do
+    it("will create an instance of a patron") do
+      patron = Patron.new({:name => "Tom Jones", :id => nil})
+      expect(patron.name).to(eq("Tom Jones"))
+    end
+  end
+end
